538f88c2014cc9fd5d2cab29e37ca7f673048018
Ashutosh K <81259418+KarAshutosh@users.noreply.github.com>
2022-07-10 16:59:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let errorEl = document.getElementById("error-msg")
+let savedMsg = document.getElementById("saved-msg")
+
 
 function increment() {
     count += 1
     countEl.textContent = count
     errorEl = ""
     errorEl.textContent = ""
+    savedMsg.textContent = ""
 }
 
 function save() {
@@ -16,9 +19,9 @@ function save() {
     saveEl.textContent = saveEl.textContent + countStr
     countEl.textContent = 0 //innerText alt but showing hidden (non-human readable) characters
     count = 0
-    errorEl.textContent = "No input"
+    errorEl.textContent = "No input yet"
     console.log(errorEl)
-
+    savedMsg.textContent = "Noted number"    
 }
 
 console.log("Let's start counting!")
